bd82cec371d6ced271aec153e851973b4d96e91b
Exist2Resist <exist2resist@outlook.com>
2021-04-27 09:13:10 -0600

    Move Master install.sh to dev branch

--- install.sh
+++ install.sh
@@ -7,24 +7,19 @@ ln -s /usr/share/zoneinfo/$TZ /etc/localtime
 ##Startup Script to Change UID and GUI in container
 cat <<'EOT' > /usr/local/bin/start.sh
 #!/bin/bash
-#TIMEZONE=${TZ:-America/Edmonton}
-#SYSTEMTZ=$(timedatectl | grep "Time zone" | awk -F':' '{ print $2 }' | awk -F'(' '{ print $1 }')
-
 rm -rf /etc/localtime
 ln -s /usr/share/zoneinfo/$TZ /etc/localt
 
-USERID=${PUID:-99}
-GROUPID=${PGID:-100}
+#USERID=${PUID:-99}
+#GROUPID=${PGID:-100}
 
-groupmod -g $GROUPID users
-usermod -u $USERID nobody
-usermod -g $USERID nobody
-usermod -d /home nobody
-chown -R nobody:users /config /opt/sabnzbd
+groupmod -g $PGID users
+usermod -u $PUID -g $PGID sabnzbd
+usermod -d /home sabnzbd
+chown -R sabnzbd:users /config /opt/sabnzbd
 chmod -R 755 /config
-
-pip3 install -q sabyenc --upgrade
 EOT
+chmod +x /usr/local/bin/start.sh
 
 ##Create Startup service for the above script
 cat <<'EOT' > /etc/systemd/system/startup.service
@@ -38,17 +33,18 @@ ExecStart=/usr/local/bin/start.sh
 TimeoutStartSec=0
 
 [Install]
-WantedBy=default.target
+WantedBy=multi-user.target
 EOT
 
 ##SABnzbd service file
 cat <<'EOT' > /etc/systemd/system/sabnzbd.service
 [Unit]
 Description=SABnzbd Daemon
+After=startup.service
 
 [Service]
 Type=forking
-User=nobody
+User=sabnzbd
 Group=users
 ExecStart=/usr/bin/python3 /opt/sabnzbd/SABnzbd.py --daemon --config-file=/config/sabnzbd_config.ini -s 0.0.0.0
 GuessMainPID=no
@@ -63,15 +59,19 @@ EOT
 #rpm gpg check is broken
 
 ##Install prerequisites
-
 dnf install -y epel-release --nogpgcheck && dnf clean all -y
 dnf install -y par2cmdline wget gcc git p7zip p7zip-plugins unzip --nogpgcheck && dnf clean all -y
 
 #Clone sabnzbd and install requirements
 cd /opt 
 git clone https://github.com/sabnzbd/sabnzbd.git
+cd /opt/sabnzbd
+git checkout master
 python3 -m pip install --upgrade pip
 python3 -m pip install -r /opt/sabnzbd/requirements.txt -U
+
+##Multi Language support
+python3 tools/make_mo.py
 
 ##Find the latest version of RAR
 RAR=$(curl -s https://www.rarlab.com/download.htm | awk -F'/rar/' '/rarlinux-x64/ { print $2 } ' | awk -F'\">' 'END {print $1}')
