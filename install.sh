b1476e11a97b7efecf320c90b07c3892b330dbe4
Exist2Resist <exist2resist@outlook.com>
2024-09-23 15:44:04 -0600

    fix file name

--- install.sh
+++ install.sh
@@ -70,11 +70,11 @@ dnf install -y python39 openssl par2cmdline automake make wget gcc gcc-c++ git p
 
 ##Replace the git clone with direct binary installation as it is failing
 cd /tmp
-xz -dv par2cmdline-turbo-v1.1.0-linux-amd64.xz
-chmod +x par2cmdline-turbo-v1.1.0-linux-amd64
+xz -dv /tmp/par2cmdline-turbo-v1.1.1-linux-amd64.xz
+chmod +x par2cmdline-turbo-v1.1.1-linux-amd64
 sudo mv /usr/bin/par2 /usr/bin/par2.old
-sudo cp par2cmdline-turbo-v1.1.0-linux-amd64 /usr/bin/par2
-rm -f par2cmdline-turbo-v1.1.1-linux-amd64.xz par2cmdline-turbo-v1.1.0-linux-amd64
+sudo cp par2cmdline-turbo-v1.1.1-linux-amd64 /usr/bin/par2
+rm -f par2cmdline-turbo-v1.1.1-linux-amd64.xz par2cmdline-turbo-v1.1.1-linux-amd64
 par2 --version 
 
 ##Clone sabnzbd and install requirements
